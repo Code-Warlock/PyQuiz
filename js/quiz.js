c28fc45393536a30c25916e9d226d227f4102b44
Code-Warlock <dessymichaeltgp123@gmail.com>
2022-04-25 10:33:43 +0100

    First Completion

--- js/quiz.js
+++ js/quiz.js
@@ -2,7 +2,7 @@
 const Questions = [{
   title: 'Who created the PEP20?',
   answers: ['Tim Berners Lee', 'Robert', 'John Murphy', 'Tim Peters'],
-  value: 'A'
+  value: 'D'
 }, {
   title: 'PEP20 is also known as ...?',
   answers: ['The Zen of Python', 'The foobar to Python', 'PyDocs of PEP', 'Module Stratarium'],
@@ -15,66 +15,90 @@ const Questions = [{
   title: 'Python was named from ...?',
   answers: ['JAVA', 'CoffeeScript', 'A comedy show', 'A snake'],
   value: 'C'
-},{
+}, {
   title: 'What keyword is used to create a variable',
-  answers: ['JAVA', 'CoffeeScript', 'A comedy show', 'A snake'],
-  value: 'C'
+  answers: ['var', 'datatype variable name', 'val', 'no keyword is used'],
+  value: 'D'
 }];
 
 
-let correctAnswers = ['A'].concat(Questions.map(item => item.value));
-let userAnswers = JSON.parse(sessionStorage.getItem('userAnswers'));
+const correctAnswers = ['A'].concat(Questions.map(item => item.value));
+const userAnswers = [];
 const WRAPPER = document.querySelector('.quiz-content');
-let quizGroup = WRAPPER.querySelector('.quiz-group');
-let quizheader = quizGroup.querySelector('.quiz-heading');
-let result = [];
+const quizGroup = WRAPPER.querySelector('.quiz-group');
+const quizheader = quizGroup.querySelector('.quiz-heading');
+const divScore = document.querySelector('.score');
+const result = [];
 
 const FORM = document.querySelector('form');
-let answerDivs = FORM.querySelectorAll('.answer-group');
-const BUTTON = FORM.querySelector('button');
+const answerDivs = FORM.querySelectorAll('.answer-group');
+const PREV = FORM.querySelector('button');
+const NEXT = FORM.querySelector('button[type*="mit"');
 var count = 0;
+const SCORETEXT = document.querySelector('.score-reveal');
 FORM.addEventListener('submit', e => {
   e.preventDefault();
   userAnswers.splice(count, 1, FORM.one.value);
   e.target.action = '';
-  sessionStorage.setItem('userAnswers', JSON.stringify(userAnswers));
-  if (count <= Questions.length) {
+  localStorage.setItem('userAnswers', JSON.stringify(userAnswers));
+  if (count < Questions.length) {
     if (!(count < 0)) {
       if (FORM.one.value == correctAnswers[count]) {
         result.push(FORM.one.value);
       }
       answerDivs.forEach((div, index) => {
-        let label = div.querySelector('label');
-        let input = div.querySelector('input');
-        let eachAnswerList = Questions[count].answers;
+        const label = div.querySelector('label');
+        const input = div.querySelector('input');
+        const eachAnswerList = Questions[count].answers;
         quizheader.textContent = Questions[count].title;
         label.textContent = eachAnswerList[index]
       });
-      console.log(userAnswers);
       ++count;
-      console.log(count);
     }
+  } else if (count == Questions.length) {
+    NEXT.textContent = 'Submit';
+    localStorage.setItem('result', result.length);
+  } else {
+    document.location.reload();
   }
 });
-BUTTON.addEventListener('click', e => {
+PREV.addEventListener('click', e => {
   count = count - 1;
   if (count > 0) {
     answerDivs.forEach((div, index) => {
-      let label = div.querySelector('label');
-      let input = div.querySelector('input');
-      let eachAnswerList = Questions[count - 1].answers;
+      const label = div.querySelector('label');
+      const input = div.querySelector('input');
+
+      const eachAnswerList = Questions[count - 1].answers;
       quizheader.textContent = Questions[count - 1].title;
       label.textContent = eachAnswerList[index]
     })
-    console.log(count);
-    sessionStorage.setItem('userAnswers', JSON.stringify(userAnswers));
+    localStorage.setItem('userAnswers', JSON.stringify(userAnswers));
     userAnswers.splice(count + 1, 1, FORM.one.value);
+    NEXT.textContent = 'Next';
   } else if (count == 0) {
     userAnswers.splice(count + 1, 1, FORM.one.value);
-    sessionStorage.setItem('userAnswers', JSON.stringify(userAnswers));
-    sessionStorage.setItem('count', count);
-    window.location.href = '';
-    count = sessionStorage.getItem('count');
+    localStorage.setItem('userAnswers', JSON.stringify(userAnswers));
+    localStorage.setItem('count', count);
+    window.location.reload();
+    count = localStorage.getItem('count');
   }
-  console.log(userAnswers);
+});
+NEXT.addEventListener('click', e => {
+  if (e.target.textContent.toLowerCase() == 'submit') {
+    divScore.classList.add('score');
+    divScore.classList.remove('remove');
+
+    divScore.style.cssText = 'display: flex;'
+    console.log(localStorage.getItem('result'))
+    SCORETEXT.textContent = Math.round((localStorage.getItem('result') / Questions.length) * 100);
+  }
+
+});
+divScore.addEventListener('click', e => {
+  divScore.classList.remove('score');
+  divScore.classList.add('remove');
+  setTimeout(() => {
+    divScore.style.cssText = 'display: none;'
+  }, 800)
 });
